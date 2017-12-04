686e224897e1aa4bd71412cbde1100f598cc6532
TuanDang <dangtuannoone@gmail.com>
2017-12-04 22:49:59 +0700

    sdfdfs

---
+++ WebThiB1Online/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebThiB1Online.MvcApplication" Language="C#" %>
---
+++ WebThiB1Online/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebThiB1Online.Startup))]
+namespace WebThiB1Online
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
