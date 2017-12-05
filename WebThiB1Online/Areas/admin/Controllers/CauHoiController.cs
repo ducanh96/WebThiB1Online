b780e758590f2fb06423ef85b9b16143108204a9
TuanDang <dangtuannoone@gmail.com>
2017-12-06 01:08:05 +0700

    hiển thị ds câu hỏi phần nghe part 1

---
+++ WebThiB1Online/Areas/admin/Controllers/CauHoiController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebThiB1Online.Areas.admin.Models.UI;
+
+namespace WebThiB1Online.Areas.admin.Controllers
+{
+    public class CauHoiController : Controller
+    {
+       // CauHoiUI UI = new CauHoiUI();
+        // GET: admin/CauHoi
+        public ActionResult Index()
+        {
+            ViewData["lstDocPart1"] = CauHoiUI.getDSCauHoiDocPart1();
+            return View();
+        }
+        public ActionResult Nghe()
+        {
+            ViewData["lstDocPart1"] = CauHoiUI.getDSCauHoiDocPart1();
+            return View();
+        }
+        public ActionResult Delete(int ID)
+        {
+            bool check = CauHoiUI.delete(ID);
+            if (check)
+            {
+                ViewBag.msgDelete = 1;
+            }
+            else
+            {
+                ViewBag.msgDelete = 0;
+            }
+            
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ WebThiB1Online/Areas/admin/Models/model/User.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebThiB1Online.Models
+{
+    public class User
+    {
+        public string ten { set; get; }
+    }
+}
