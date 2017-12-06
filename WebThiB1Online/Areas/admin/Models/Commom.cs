481c7dfff421f4b5987ae79ea991cf7142b93724
ducanh <rogerducanh@gmail.com>
2017-12-06 21:25:41 +0700

    add tam thơi

---
+++ WebThiB1Online/Areas/admin/Models/Commom.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebThiB1Online.Areas.admin.Models
+{
+    public class Commom
+    {
+        public const  string DataPart1 = "lstDocPart1";
+    }
+}
