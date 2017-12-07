b3801528133a3f145878e55b927e34fc7a5dc4a6
phongtung96 <phongtung96@gmail.com>
2017-12-07 23:16:11 +0700

    Giao diện chủ đề

---
+++ WebThiB1Online/Areas/admin/Controllers/ChuDeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebThiB1Online.Areas.admin.Models.UI;
+namespace WebThiB1Online.Areas.admin.Controllers
+{
+    public class ChuDeController : Controller
+    {
+        //
+        // GET: /admin/ChuDe/
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Chude()
+        {
+            return View();
+        }
+        public ActionResult Delete(int ID)
+        {
+            bool check = ChuDeUI.delete(ID);
+            if (check)
+            {
+                ViewBag.msgDelete = 1;
+            }
+            else
+            {
+                ViewBag.msgDelete = 0;
+            }
+
+            return RedirectToAction("Chude");
+        }
+	}
+}
